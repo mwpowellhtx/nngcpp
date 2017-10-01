6d8dbd579373d2896dcd847e03ac4ec4c2967309
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-01 15:58:51 -0400

    do not need to preface the ctor call in fact is probably better not to

--- swig/cpp/src/core/nng_exception.cpp
+++ swig/cpp/src/core/nng_exception.cpp
@@ -2,16 +2,16 @@
 
 namespace nng {
 
-    nng_exception::nng_exception() : std::exception() {
+    nng_exception::nng_exception() : exception() {
         error_code = ec_eunknown;
     }
 
-    nng_exception::nng_exception(int ec) : std::exception() {
+    nng_exception::nng_exception(int ec) : exception() {
         error_code = static_cast<error_code_type>(ec);
         message = ::nng_strerror(ec);
     }
 
-    nng_exception::nng_exception(error_code_type ec) : std::exception() {
+    nng_exception::nng_exception(error_code_type ec) : exception() {
         error_code = ec;
         message = ::nng_strerror(static_cast<int>(ec));
     }
