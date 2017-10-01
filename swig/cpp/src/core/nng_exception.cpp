8f7c91d6f4b43d82d552de54c85e91edd76992fe
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-01 15:36:29 -0400

    fleshed out some error codes

--- swig/cpp/src/core/nng_exception.cpp
+++ swig/cpp/src/core/nng_exception.cpp
@@ -3,11 +3,17 @@
 namespace nng {
 
     nng_exception::nng_exception() : std::exception() {
-        error_code = -1;
+        error_code = ec_eunknown;
     }
 
     nng_exception::nng_exception(int ec) : std::exception() {
-        message = ::nng_strerror(error_code = ec);
+        error_code = static_cast<error_code_type>(ec);
+        message = ::nng_strerror(ec);
+    }
+
+    nng_exception::nng_exception(error_code_type ec) : std::exception() {
+        error_code = ec;
+        message = ::nng_strerror(static_cast<int>(ec));
     }
 
     nng_exception::~nng_exception() {        
--- swig/cpp/src/core/nng_exception.h
+++ swig/cpp/src/core/nng_exception.h
@@ -9,10 +9,43 @@
 
 namespace nng {
 
+    // https://www.gnu.org/software/libc/manual/html_node/Error-Codes.html
+    enum error_code_type {
+        ec_eunknown = -1,
+        ec_eintr = ::NNG_EINTR,
+        ec_enomem = ::NNG_ENOMEM,
+        ec_einval = NNG_EINVAL,
+        ec_ebusy = NNG_EBUSY,
+        ec_etimedout = NNG_ETIMEDOUT,
+        ec_econnrefused = NNG_ECONNREFUSED,
+        ec_eclosed = NNG_ECLOSED,
+        ec_eagain = NNG_EAGAIN,
+        ec_enotsup = NNG_ENOTSUP,
+        ec_eaddrinuse = NNG_EADDRINUSE,
+        ec_estate = NNG_ESTATE,
+        ec_enoent = NNG_ENOENT,
+        ec_eproto = NNG_EPROTO,
+        ec_eunreachable = NNG_EUNREACHABLE,
+        ec_eaddrinval = NNG_EADDRINVAL,
+        ec_eperm = NNG_EPERM,
+        ec_emsgsize = NNG_EMSGSIZE,
+        ec_econnaborted = NNG_ECONNABORTED,
+        ec_econnreset = NNG_ECONNRESET,
+        ec_ecanceled = NNG_ECANCELED,
+        ec_enofiles = NNG_ENOFILES,
+        ec_enospc = NNG_ENOSPC,
+        ec_eexist = NNG_EEXIST,
+        ec_ereadonly = NNG_EREADONLY,
+        ec_ewriteonly = NNG_EWRITEONLY,
+        ec_einternal = NNG_EINTERNAL,
+        ec_esyserr = NNG_ESYSERR,
+        ec_etranerr = NNG_ETRANERR,
+    };
+
     class nng_exception : public std::exception {
         public:
 
-            int error_code;
+            error_code_type error_code;
 
             std::string message;
 
@@ -21,6 +54,8 @@ namespace nng {
             nng_exception();
 
             nng_exception(int error_code);
+
+            nng_exception(error_code_type error_code);
 
             virtual ~nng_exception();
     };
