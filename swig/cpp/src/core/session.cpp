953118b3d170f462db3cfb766bcf2ba492af52bf
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-05 18:54:47 -0400

    added resource managers create and remove to the API

--- swig/cpp/src/core/session.cpp
+++ swig/cpp/src/core/session.cpp
@@ -1,11 +1,60 @@
 #include "session.h"
+#include "core/listener.h"
+
+#include <algorithm>
 
 namespace nng {
 
-    session::session() {
+    session::session()
+        : _push_sockets()
+        , _pull_sockets()
+        , _listeners() {
+    }
+
+    template<class Type_>
+    void __release_all(std::vector<std::shared_ptr<Type_>>& values) {
+        for (auto & x : values) {
+            x.reset();
+        }
+        values.clear();
     }
 
     session::~session() {
+        __release_all(_push_sockets);
+        __release_all(_pull_sockets);
+        __release_all(_listeners);
         ::nng_fini();
+    }
+
+    template<class Type_>
+    std::shared_ptr<Type_> __create(std::vector<std::shared_ptr<Type_>>& values) {
+        auto sp = std::make_shared<Type_>();
+        values.push_back(sp);
+        return sp;
+    }
+
+    template<class Type_>
+    void __remove(std::vector<std::shared_ptr<Type_>>& values, const Type_* const valuep) {
+        const auto __where= [&](std::shared_ptr<Type_> x) {
+            return x.get() == valuep;
+        };
+        std::remove_if(values.begin(), values.end(), __where);
+    }
+
+    std::shared_ptr<protocol::latest_push_socket> session::create_push_socket() {
+        return __create(_push_sockets);
+    }
+
+    std::shared_ptr<protocol::latest_pull_socket> session::create_pull_socket() {
+        return __create(_pull_sockets);
+    }
+
+    std::shared_ptr<listener> session::create_listener() {
+        return __create(_listeners);
+    }
+
+    void session::remove_listener(const listener* const lp) {
+        // Yes, in fact, we do not need the full smart pointer in this instance.
+        __remove(_listeners, lp);
     }
 }
--- swig/cpp/src/core/session.h
+++ swig/cpp/src/core/session.h
@@ -2,16 +2,36 @@
 #define NNGCPP_SESSION_H
 
 #include "../nngcpp_integration.h"
+#include "core/options.h"
+#include "protocol/protocol.h"
+
+#include <vector>
+#include <memory>
 
 // TODO: TBD: considering the NNG fini teardown, it may be appropriate to emit appropriately protocoled sockets from the session as well...
 namespace nng {
 
+    class listener;
+
     class session {
+        private:
+
+            std::vector<std::shared_ptr<protocol::latest_push_socket>> _push_sockets;
+            std::vector<std::shared_ptr<protocol::latest_pull_socket>> _pull_sockets;
+
+            std::vector<std::shared_ptr<listener>> _listeners;
+
         public:
 
             session();
 
             virtual ~session();
+
+            std::shared_ptr<protocol::latest_push_socket> create_push_socket();
+            std::shared_ptr<protocol::latest_pull_socket> create_pull_socket();
+
+            std::shared_ptr<listener> create_listener();
+            void remove_listener(const listener* const lp);
     };
 }
 
