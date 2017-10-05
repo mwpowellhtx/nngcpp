17fe103f13d9fae6079510f0b0d9fc10e761157e
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-05 18:55:09 -0400

    added notes concerning NNG pipe nomenclature

--- swig/cpp/src/core/endpoint.h
+++ swig/cpp/src/core/endpoint.h
@@ -5,6 +5,7 @@
 
 namespace nng {
 
+    // TODO: TBD: carries along with it information about ::nng_pipe ... just a matter of how to present it to the C++ (or SWIG) community...
     class endpoint {
         protected:
 
