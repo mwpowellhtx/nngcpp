b22f078b5d2efd494304c8e859c521d6be5f7554
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-06 18:27:22 -0400

    did some fleshing out around the session interfaces

--- swig/cpp/src/core/session.cpp
+++ swig/cpp/src/core/session.cpp
@@ -1,14 +1,15 @@
 #include "session.h"
-#include "core/listener.h"
 
 #include <algorithm>
 
 namespace nng {
 
     session::session()
-        : _push_sockets()
-        , _pull_sockets()
-        , _listeners() {
+        : _dialer_eps()
+        , _listener_eps()
+        , _pair_sockets()
+        , _push_sockets()
+        , _pull_sockets() {
     }
 
     template<class Type_>
@@ -20,25 +21,61 @@ namespace nng {
     }
 
     session::~session() {
+        __release_all(_dialer_eps);
+        __release_all(_listener_eps);
+        __release_all(_pair_sockets);
         __release_all(_push_sockets);
         __release_all(_pull_sockets);
-        __release_all(_listeners);
         ::nng_fini();
     }
 
-    template<class Type_>
-    std::shared_ptr<Type_> __create(std::vector<std::shared_ptr<Type_>>& values) {
-        auto sp = std::make_shared<Type_>();
+    template<class Type_, typename ...Args_>
+    std::shared_ptr<Type_> __create(std::vector<std::shared_ptr<Type_>>& values, Args_ &&...args) {
+        auto sp = std::make_shared<Type_>(args...);
         values.push_back(sp);
         return sp;
     }
 
     template<class Type_>
     void __remove(std::vector<std::shared_ptr<Type_>>& values, const Type_* const valuep) {
-        const auto __where= [&](std::shared_ptr<Type_> x) {
+        const auto __where = [&](std::shared_ptr<Type_> x) {
             return x.get() == valuep;
         };
         std::remove_if(values.begin(), values.end(), __where);
+    }
+
+    std::shared_ptr<dialer> session::create_dialer_ep() {
+        return __create(_dialer_eps);
+    }
+
+    std::shared_ptr<dialer> session::create_dialer_ep(const socket& s, const std::string& addr) {
+        return __create(_dialer_eps, s, addr);
+    }
+
+    std::shared_ptr<listener> session::create_listener_ep() {
+        return __create(_listener_eps);
+    }
+
+    std::shared_ptr<listener> session::create_listener_ep(const socket& s, const std::string& addr) {
+        return __create(_listener_eps, s, addr);
+    }
+
+    void session::remove_dialer_ep(const dialer* const dp) {
+        // Yes, in fact, we do not need the full smart pointer in this instance.
+        __remove(_dialer_eps, dp);
+    }
+
+    void session::remove_listener_ep(const listener* const lp) {
+        // Ditto smart pointers.
+        __remove(_listener_eps, lp);
+    }
+
+    std::shared_ptr<protocol::latest_pair_socket> session::create_pair_socket() {
+        return __create(_pair_sockets);
+    }
+
+    void session::remove_pair_socket(const protocol::latest_pair_socket* const sp) {
+        __remove(_pair_sockets, sp);
     }
 
     std::shared_ptr<protocol::latest_push_socket> session::create_push_socket() {
@@ -47,14 +84,5 @@ namespace nng {
 
     std::shared_ptr<protocol::latest_pull_socket> session::create_pull_socket() {
         return __create(_pull_sockets);
-    }
-
-    std::shared_ptr<listener> session::create_listener() {
-        return __create(_listeners);
-    }
-
-    void session::remove_listener(const listener* const lp) {
-        // Yes, in fact, we do not need the full smart pointer in this instance.
-        __remove(_listeners, lp);
     }
 }
--- swig/cpp/src/core/session.h
+++ swig/cpp/src/core/session.h
@@ -3,23 +3,27 @@
 
 #include "../nngcpp_integration.h"
 #include "core/options.h"
+#include "core/listener.h"
+#include "core/dialer.h"
 #include "protocol/protocol.h"
 
-#include <vector>
 #include <memory>
+#include <vector>
 
 // TODO: TBD: considering the NNG fini teardown, it may be appropriate to emit appropriately protocoled sockets from the session as well...
 namespace nng {
 
-    class listener;
 
     class session {
         private:
 
+            std::vector<std::shared_ptr<dialer>> _dialer_eps;
+            std::vector<std::shared_ptr<listener>> _listener_eps;
+
+            std::vector<std::shared_ptr<protocol::latest_pair_socket>> _pair_sockets;
+
             std::vector<std::shared_ptr<protocol::latest_push_socket>> _push_sockets;
             std::vector<std::shared_ptr<protocol::latest_pull_socket>> _pull_sockets;
-
-            std::vector<std::shared_ptr<listener>> _listeners;
 
         public:
 
@@ -27,11 +31,20 @@ namespace nng {
 
             virtual ~session();
 
+            std::shared_ptr<dialer> create_dialer_ep();
+            std::shared_ptr<dialer> create_dialer_ep(const socket& s, const std::string& addr);
+
+            std::shared_ptr<listener> create_listener_ep();
+            std::shared_ptr<listener> create_listener_ep(const socket& s, const std::string& addr);
+
+            void remove_dialer_ep(const dialer* const dp);
+            void remove_listener_ep(const listener* const lp);
+
+            std::shared_ptr<protocol::latest_pair_socket> create_pair_socket();
+            void remove_pair_socket(const protocol::latest_pair_socket* const sp);
+
             std::shared_ptr<protocol::latest_push_socket> create_push_socket();
             std::shared_ptr<protocol::latest_pull_socket> create_pull_socket();
-
-            std::shared_ptr<listener> create_listener();
-            void remove_listener(const listener* const lp);
     };
 }
 
