8a23bdd2291eea6436115919877b355e76b9b83f
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-10 10:40:51 -0400

    fleshed out the scalability testing which is failing on a timeout additionally Catch upon which testing is currently based is not fully thread-safe

--- swig/cpp/tests/core/scalability.cpp
+++ swig/cpp/tests/core/scalability.cpp
@@ -1,4 +1,5 @@
 //
+// Copyright (c) 2017 Michael W Powell <mwpowellhtx@gmail.com>
 // Copyright 2017 Garrett D'Amore <garrett@damore.org>
 // Copyright 2017 Capitar IT Group BV <info@capitar.com>
 //
@@ -8,13 +9,17 @@
 // found online at https://opensource.org/licenses/MIT.
 //
 
-#include <catch.hpp>
+#include "../catch/catch_exception_translations.hpp"
 
 #include <nngcpp.h>
 
 #include <string>
 #include <thread>
 #include <functional>
+#include <algorithm>
+#include <chrono>
+#include <ostream>
+#include <atomic>
 
 #include "../catch/catch_macros.hpp"
 
@@ -32,24 +37,46 @@ namespace nng {
 
         std::unique_ptr<std::thread> _server_threadp;
 
+        std::atomic<bool> _stop_server;
+
         // TODO: TBD: listen here? or just prior to entering the thread?
-        static void server_callback(std::shared_ptr<protocol::latest_rep_socket> sp) {
+        void server_callback(std::shared_ptr<protocol::latest_rep_socket> sp) {
 
-            using namespace constants;
+            using namespace std;
+            using namespace protocol;
 
-            //// TODO: TBD: Catch v1 is apparently known thread-unsafe; will be interested to know if/when v2 will be ready for prime time
-            //REQUIRE_NOTHROW(sp->listen(atscale_addr));
-            //// TODO: TBD: in the meanwhile, it may be interested to consider alternatives...
-            //// https://github.com/ned14/quickcpplib/blob/master/include/boost/test/unit_test.hpp
-            //// https://ned14.github.io/quickcpplib/hierarchy.html
-            sp->listen(atscale_addr);
+            //INFO("Server: started...");
+
+            while (!_stop_server) {
+
+                string r;
+                latest_rep_socket::receive_size_type sz = 128;
+
+                //INFO("Server: receiving...");
+
+                REQUIRE_NOTHROW(sp->try_receive(r, sz));
+
+                //INFO("Server: received '" + r + "'");
+
+                const auto s = r;
+
+                //INFO("Server: sending '" + s + "'");
+
+                REQUIRE_NOTHROW(sp->send(s));
+
+                //INFO("Server: sent.");
+            }
+
+            //INFO("Server: stopped.");
         }
 
         void init() {
 
+            _stop_server = false;
+
             using _opt_ = option_names;
 
-            INFO("Setting up server reply socket.");
+            //INFO("Setting up server reply socket.");
 
             REQUIRE_NOTHROW(_repsp = _sessionp->create_rep_socket());
 
@@ -59,9 +86,17 @@ namespace nng {
             REQUIRE_NOTHROW(_repsp->set_option_int(_opt_::send_buffer, 256));
 
             using namespace std::placeholders;
+            using namespace constants;
 
-            _server_threadp = std::make_unique<std::thread>(
-                std::bind(&server_callback, _1), _repsp);
+            //// TODO: TBD: Catch v1 is apparently known thread-unsafe; will be interested to know if/when v2 will be ready for prime time
+            //REQUIRE_NOTHROW(sp->listen(atscale_addr));
+            //// TODO: TBD: in the meanwhile, it may be interested to consider alternatives...
+            //// https://github.com/ned14/quickcpplib/blob/master/include/boost/test/unit_test.hpp
+            //// https://ned14.github.io/quickcpplib/hierarchy.html
+            _repsp->listen(atscale_addr);
+
+            _server_threadp = std::make_unique<std::thread>(std::bind(
+                &server_simulation::server_callback, this, _1), _repsp);
         }
 
     public:
@@ -72,83 +107,88 @@ namespace nng {
         }
 
         ~server_simulation() {
+            // TODO: requires to signal the thread?
+            //INFO("Server: stopping...");
+            _stop_server = true;
+            //INFO("Server: thread rejoining...");
             _server_threadp->join();
+            //INFO("Server: thread rejoined.");
             _sessionp->remove_rep_socket(_repsp.get());
         }
     };
 }
 
-//static int nclients = 200;
+#define SOCKET_COUNT 200
 
-nng_socket   rep;
-void *       server;
+typedef std::vector<std::shared_ptr<nng::protocol::latest_req_socket>> req_socket_vector;
 
-void
-serve(void *arg)
-{
-	nng_msg *msg;
+req_socket_vector create_and_dial_clients(nng::session& session, int count = SOCKET_COUNT) {
 
-	for (;;) {
-		msg = NULL;
-		if ((nng_recvmsg(rep, &msg, 0) != 0) ||
-		    (nng_sendmsg(rep, msg, 0) != 0)) {
-			break;
-		}
-	}
-	if (msg != NULL) {
-		nng_msg_free(msg);
-	}
-	nng_close(rep);
+    using namespace std;
+    using namespace nng;
+    using namespace nng::protocol;
+    using namespace constants;
+    using _opt_ = option_names;
+
+    static const auto timeout = 100ms;
+
+    req_socket_vector sockets;
+
+    while (count--) {
+
+        shared_ptr<latest_req_socket> sp;
+
+        REQUIRE_NOTHROW(sp = session.create_req_socket());
+        REQUIRE(sp.get() != nullptr);
+
+        REQUIRE_NOTHROW(sp->set_option_usec(_opt_::receive_timeout_usec, timeout.count()));
+        REQUIRE_NOTHROW(sp->set_option_usec(_opt_::send_timeout_usec, timeout.count()));
+
+        REQUIRE_NOTHROW(sp->dial(atscale_addr));
+
+        sockets.push_back(sp);
+    }
+
+    return sockets;
 }
 
-void
-stop(void)
-{
-	nng_closeall();
-	nng_thread_destroy(server);
-	//nng_fini();
-}
+bool transact_clients(req_socket_vector& sockets) {
 
-int
-openclients(nng_socket *clients, int num)
-{
-	int      rv;
-	int      i;
-	uint64_t t;
-	for (i = 0; i < num; i++) {
-		t = 100000; // 100ms
-		nng_socket c;
-		if (((rv = nng_req_open(&c)) != 0) ||
-		    ((rv = nng_setopt_usec(c, NNG_OPT_RECVTIMEO, t)) != 0) ||
-		    ((rv = nng_setopt_usec(c, NNG_OPT_SENDTIMEO, t)) != 0) ||
-		    ((rv = nng_dial(c, addr, NULL, 0)) != 0)) {
-			return (rv);
-		}
-		clients[i] = c;
-	}
-	return (0);
-}
+    using namespace std;
+    using namespace nng;
+    using namespace nng::protocol;
 
-int
-transact(nng_socket *clients, int num)
-{
-	nng_msg *msg;
-	int      rv;
-	int      i;
+    auto value = static_cast<int>(1);
 
-	for (i = 0; i < num; i++) {
+    const auto get_next_message = [&]() {
+        ostringstream os;
+        os << ++value;
+        return string(os.str());
+    };
 
-		if (((rv = nng_msg_alloc(&msg, 0)) != 0) ||
-		    ((rv = nng_sendmsg(clients[i], msg, 0)) != 0) ||
-		    ((rv = nng_recvmsg(clients[i], &msg, 0)) != 0)) {
-			// We may leak a message, but this is an
-			// error case anyway.
-			break;
-		}
-		nng_msg_free(msg);
-		msg = NULL;
-	}
-	return (rv);
+    for_each(sockets.begin(), sockets.end(), [&](shared_ptr<latest_req_socket> sp) {
+
+        // TODO: TBD: um, okay... this seems like it should be a stronger error then: i.e. REQUIRE_NOTHROW, instead of simply a "break"
+        // We may leak a message, but this is an error case anyway.
+        const auto s = get_next_message(), expected = s;
+
+        //INFO("Client: sending '" + s + "'");
+
+        REQUIRE_NOTHROW(sp->send(s));
+
+        //INFO("Client: sent '" + s + "'");
+
+        string r;
+        auto sz = s.size();
+
+        //INFO("Client: receiving...");
+
+        REQUIRE_NOTHROW(sp->try_receive(r, sz));
+
+        //INFO("Client: received '" + r + "'");
+    });
+
+    return true;
 }
 
 TEST_CASE("Test that messaging scales", "[scalability]") {
@@ -160,35 +200,23 @@ TEST_CASE("Test that messaging scales", "[scalability]") {
     session _session_;
     server_simulation _sim_(&_session_);
 
-    nng_socket *clients;
-	int *       results;
+    // Because of the nature of SECTION, I'm not sure this is quite the right model for this test.
+    //INFO("We can handle many, many clients.");
 
-	atexit(stop);
+    {
+        auto sockets = create_and_dial_clients(_session_);
 
-	clients = calloc(nclients, sizeof(nng_socket));
-	results = calloc(nclients, sizeof(int));
+        REQUIRE(sockets.size() == SOCKET_COUNT);
 
-    //// TODO: TBD: all this is pretty much taken care of on the front end of the Server Simulation during its ctor.
-	//if (/*(nng_rep_open(&rep) != 0) ||*/
-	//    /*(nng_setopt_int(rep, NNG_OPT_RECVBUF, 256) != 0) ||*/
-	//    /*(nng_setopt_int(rep, NNG_OPT_SENDBUF, 256) != 0) ||*/
-	//    /*(nng_listen(rep, addr, NULL, 0) != 0) ||*/
-	//    (nng_thread_create(&server, serve, NULL) != 0)) {
-	//	fprintf(stderr, "Unable to set up server!\n");
-	//	exit(1);
-	//}
+        transact_clients(sockets);
 
-	Test("Scalability", {
-		Convey("We can handle many many clients", {
-			int i;
-			So(openclients(clients, nclients) == 0);
-			So(transact(clients, nclients) == 0);
-			for (i = 0; i < nclients; i++) {
-				So(nng_close(clients[i]) == 0);
-			}
-		});
-	});
+        for (auto& sp : sockets) {
 
-	free(clients);
-	free(results);
-});
+            _session_.remove_req_socket(sp.get());
+
+            remove_if(sockets.begin(), sockets.end(), [&sp](req_socket_vector::value_type& x) {
+                return x.get() == sp.get();
+            });
+        }
+    }
+}
