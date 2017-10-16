bbccb46c2bde75fcc72378fd1d917b2728cfb18a
Michael W Powell <mwpowellhtx@gmail.com>
2017-10-16 11:36:36 -0400

    updated constants definitions added implementation to go with the inclusion

---
+++ swig/cpp/tests/helpers/constants.cpp
@@ -0,0 +1,14 @@
+#include "constants.h"
+
+#include <algorithm>
+
+namespace constants {
+
+    nng::messaging::binary_message::buffer_vector_type to_buffer(const std::string& s) {
+        nng::messaging::binary_message::buffer_vector_type buf;
+        std::for_each(s.cbegin(), s.cend(), [&buf](const std::string::value_type& ch) {
+            buf.push_back(ch);
+        });
+        return buf;
+    }
+}
--- swig/cpp/tests/helpers/constants.h
+++ swig/cpp/tests/helpers/constants.h
@@ -3,19 +3,19 @@
 
 #include <nngcpp.h>
 
-#include <algorithm>
+#include <ostream>
 
 namespace constants {
 
     // TODO: TBD: this really deserves first class support throughout test framework
-    nng::messaging::binary_message::buffer_vector_type to_buffer(const std::string& s) {
-        nng::messaging::binary_message::buffer_vector_type buf;
-        std::for_each(s.cbegin(), s.cend(), [&buf](const std::string::value_type& ch) {
-            buf.push_back(ch);
-        });
-        return buf;
-    }
+    nng::messaging::binary_message::buffer_vector_type to_buffer(const std::string& s);
 
+    template<typename Type_>
+    std::string to_str(Type_ value) {
+        std::ostringstream os;
+        os << value;
+        return os.str();
+    }
 }
 
 #endif // NNGCPP_TESTS_CONSTANTS_HELPERS_H
