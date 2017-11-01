1b39abcd8ade8c267e529f07f1233a699594f10a
Michael W Powell <mwpowellhtx@gmail.com>
2017-11-01 11:19:56 -0400

    added clarifying comments

--- swig/cpp/src/core/ICanDial.hpp
+++ swig/cpp/src/core/ICanDial.hpp
@@ -11,6 +11,7 @@ namespace nng {
     class _Dialer;
 #endif // NNGCPP_DIALER_H
 
+    // This is an instance where the internal externally facing API really does need to be pointer instead of reference-based.
     struct ICanDial {
         virtual void Dial(const std::string& addr, flag_type flags = flag_none) = 0;
         virtual void Dial(const std::string& addr, _Dialer* const dp, flag_type flags = flag_none) = 0;
--- swig/cpp/src/core/ICanListen.hpp
+++ swig/cpp/src/core/ICanListen.hpp
@@ -11,6 +11,7 @@ namespace nng {
     class _Listener;
 #endif // NNGCPP_LISTENER_H
 
+    // This is an instance where the internal externally facing API really does need to be pointer instead of reference-based.
     struct ICanListen {
         virtual void Listen(const std::string& addr, flag_type flags = flag_none) = 0;
         virtual void Listen(const std::string& addr, _Listener* const lp, flag_type flags = flag_none) = 0;
